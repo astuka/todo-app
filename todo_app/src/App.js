e0df742a5905c2d9467ba05aba81cc0595c0496b
astuka <jrobins855@gmail.com>
2024-08-07 08:57:50 -0700

    modified:   todo_app/src/App.js

--- todo_app/src/App.js
+++ todo_app/src/App.js
@@ -3,12 +3,19 @@ import './App.css';
 
 function App() {
   const [todos, setTodos] = useState(['task1','task2']);
+  const [input, setInput] = useState('')
+
+  const addTodo = (event) => {
+    //when button click
+    setTodos([...todos, input]);
+
+  }
   
   return (
     <div className="App">
       <h1>Todo App</h1>
-      <input />
-      <button>Add Todo</button>
+      <input value={input} onChange={event => setInput(event.target.value)}/>
+      <button onClick={addTodo}>Add Todo</button>
 
       <ul>
         {todos.map(todo => (
